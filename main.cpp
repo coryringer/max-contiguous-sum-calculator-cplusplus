7be9570ba8183650c12dd9df85aa1c14e383f9a1
Cory Ringer <36169391+coryringer@users.noreply.github.com>
2018-02-11 23:28:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,44 +12,44 @@ int main()
   int y = 750;                          // y-coord of string to PS file
   Sequence mySeq;                       // create mySeq
   PostScript file;
-	std::string psFilename = "output.ps"; // PostScript filename
+  std::string psFilename = "output.ps"; // PostScript filename
   std::ostringstream buffer;
   std::vector<std::string> heading = {"Program: Max Contiguous Sum Calculator",
-  															      "Author: Cory Ringer",
-  															      "Date: October 2017",
-  															      "Output: PostScript file (.ps)",
-  															      "Purpose: The purpose of this program is " 
-  															      "to create an array a[] filled with random numbers ",
- 		                        		      "and to determine the largest sum from a " 
-	 														        "contiguous subsequence of a[].",
- 		                    				      ""};
-	// Populate mySeq.
+  				      "Author: Cory Ringer",
+  				      "Date: October 2017",
+  				      "Output: PostScript file (.ps)",
+  				      "Purpose: The purpose of this program is " 
+  				      "to create an array a[] filled with random numbers ",
+ 		                      "and to determine the largest sum from a " 
+	 			      "contiguous subsequence of a[].",
+ 		                      ""};
+  // Populate mySeq.
   mySeq.makeArr();
 
-	// Open PostScript file for writing.
+  // Open PostScript file for writing.
   file.open(psFilename); 
 
   // Print to PostScript file.
   file.start();
   file.font("Courier", 10);
 
-	// Print contents of 'heading' to PostScript file.
+  // Print contents of 'heading' to PostScript file.
   for (auto & element : heading) { 
   	file.text(element, X_COORD, y);
   	y -= PS_NEWLINE; // skip to next line down
   }
 
-	// Print mySeq to file.
+  // Print mySeq to file.
   mySeq.print(file, X_COORD, PS_NEWLINE, &y); 
   y -= PS_NEWLINE;
   buffer << "From this array a[], " 
-  		   	<< mySeq.getMaxSubsum() 
-  		   	<< " is the largest contiguous sum.";
+  	 << mySeq.getMaxSubsum() 
+  	 << " is the largest contiguous sum.";
   file.text(buffer.str(), X_COORD, y);
   file.show();
 
-	// Close PostScript file.
-	file.close();
+  // Close PostScript file.
+  file.close();
 	
   return 0;
 }
