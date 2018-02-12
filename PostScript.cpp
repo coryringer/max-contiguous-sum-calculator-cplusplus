c7435c2cc5867c400f6f77b23c773428a7b1bbab
Cory Ringer <36169391+coryringer@users.noreply.github.com>
2018-02-11 23:26:36 -0500

    Update PostScript.cpp

--- PostScript.cpp
+++ PostScript.cpp
@@ -7,12 +7,12 @@
 void PostScript::text(std::string str, int xCoord, int yCoord)   
 {
 	psfile << xCoord << " " << yCoord << " " << "moveto"
-		     << std::endl << "(" << str << ") show" << std::endl;
+	       << std::endl << "(" << str << ") show" << std::endl;
 }
 
 // 'font' selects the font type and size for a string and prints to file.
 void PostScript::font(std::string fontName, int fontSize) 										
 {
 	psfile << "/" << fontName << std::endl << fontSize
-		     << " selectfont" << std::endl;
+	       << " selectfont" << std::endl;
 }
